8680fbbea85fad12d097fe6ec3aee62097b022a6
GreyRobin <86514645+GreyRobin@users.noreply.github.com>
2021-08-09 17:26:14 +1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -20,67 +20,31 @@ function setup() {
 		isStatic:true			
 	}
     var ball_options={
-		restitution:0.8
+		isStatic:false,
+		restitution:1,
+		friction:0,
+		density:0.8
+
 	}
-	con1 = Matter.Constraint.create({
-		pointA:{x:0,y:0},
-		bodyB:ball1,
-		pointB:{x:0,y:0},
-		bodyB:roof,
-		length:100,
-		stiffness:0.1,
-	  })
-	  World.add(world,con1)
-	  con2 = Matter.Constraint.create({
-		pointA:{x:0,y:0},
-		bodyB:ball1,
-		pointB:{x:0,y:0},
-		bodyB:roof,
-		length:100,
-		stiffness:0.1,
-	  })
-	  World.add(world,con2)
-	  con3 = Matter.Constraint.create({
-		pointA:{x:0,y:0},
-		bodyB:ball1,
-		pointB:{x:0,y:0},
-		bodyB:roof,
-		length:100,
-		stiffness:0.1,
-	  })
-	  World.add(world,con3)
-	  con4 = Matter.Constraint.create({
-		pointA:{x:0,y:0},
-		bodyB:ball1,
-		pointB:{x:0,y:0},
-		bodyB:roof,
-		length:100,
-		stiffness:0.1,
-	  })
-	  World.add(world,con4)
-	  con1 = Matter.Constraint.create({
-		pointA:{x:0,y:0},
-		bodyB:ball1,
-		pointB:{x:0,y:0},
-		bodyB:roof,
-		length:100,
-		stiffness:0.1,
-	  })
-	  World.add(world,con5)
+
 	  
 	roof = Bodies.rectangle(400,100,230,20,roof_options);
-	ball1 = Bodies.circle(300,100,20,ball_options)
-	ball2 = Bodies.circle(350,100,20,ball_options)
-	ball3 = Bodies.circle(400,100,20,ball_options)
-	ball4 = Bodies.circle(450,100,20,ball_options)
-	ball5 = Bodies.circle(500,100,20,ball_options)
+	ball1 = Bodies.circle(320,380,20,ball_options)
+	ball2 = Bodies.circle(360,380,20,ball_options)
+	ball3 = Bodies.circle(400,380,20,ball_options)
+	ball4 = Bodies.circle(440,380,20,ball_options)
+	ball5 = Bodies.circle(480,380,20,ball_options)
     World.add(world,ball1)
 	World.add(world,ball2)
 	World.add(world,ball3)
 	World.add(world,ball4)
 	World.add(world,ball5)
     World.add(world,roof);
-
+con1 = new rope(ball1,roof,-80,0)
+con2 = new rope(ball2,roof,-40,0)
+con3 = new rope(ball3,roof,0,0)
+con4 = new rope(ball4,roof,40,0)
+con5 = new rope(ball5,roof,80,0)
 	Engine.run(engine);
 	
   
@@ -93,19 +57,23 @@ function draw() {
   rect(roof.position.x,roof.position.y,230,20);
 
   //call display() to show ropes here
-
+con1.display()
+con2.display()
+con3.display()
+con4.display()
+con5.display()
   
   //create ellipse shape for multiple bobs here
-ellipse(ball1.position.x,ball1.position.y,10)
-ellipse(ball2.position.x,ball2.position.y,10)
-ellipse(ball3.position.x,ball3.position.y,10)
-ellipse(ball4.position.x,ball4.position.y,10)
-ellipse(ball5.position.x,ball5.position.y,10)
+ellipse(ball1.position.x,ball1.position.y,40,40)
+ellipse(ball2.position.x,ball2.position.y,40,40)
+ellipse(ball3.position.x,ball3.position.y,40,40)
+ellipse(ball4.position.x,ball4.position.y,40,40)
+ellipse(ball5.position.x,ball5.position.y,40,40)
 }
 
 //Write keyPressed function and apply force on pressing up_arrow key on the first bob.
 function keyPressed(){
 	if(keyCode==UP_ARROW){
-	  Matter.Body.applyForce(ball1,{x:0,y:0},{x:0.05,y:0})
+	  Matter.Body.applyForce(ball1,ball1.position,{x:-50,y:-45})
 	}
   }
